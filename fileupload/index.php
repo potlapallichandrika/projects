4bc4ef290c09ee6ac3fb95098cfffbc60f153d27
potlapallichandrika <chandrikaip4@gmail.com>
2023-08-16 15:30:23 +0530

    add file upload

---
+++ fileupload/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+  Select image to upload:
+  <input type="file" name="file" id="fileToUpload">
+  <button type="submit" value="Upload Image" name="submit">UPLOAD</button>
+</form>
+
+</body>
+</html>
+
